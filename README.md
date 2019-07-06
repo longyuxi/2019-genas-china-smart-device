e95fa07195f65ddd6518680b29fdaf6ad9aad6da
longyuxi <longyuxi@live.com>
2019-07-06 10:13:54 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # 2019-genas-china-smart-device
  
+1. Use Etcher to flash Raspbian image to the Pi.
+
+2. Add an empty file named `ssh` to the **boot partition**.  This enables the ssh daemon when it boots.
+
+3. Edit these files on the **OS partition**:
+  * Edit `/etc/hostname` and `/etc/hosts` to change “raspberrypi” to a **unique host name**, like `smart-detector-pi`.
+  * Edit `/etc/wpa_supplicant/wpa_supplicant.conf` to add your WiFi authentication (updating "country" if appropriate):
+
+```
+country=us
+update_config=1
+ctrl_interface=/var/run/wpa_supplicant
+
+network={
+ scan_ssid=1
+ ssid="your WiFi name (SSID)"
+ psk="your WiFi password"
+}
+```
+
+4. Use an IP scanner (such as the "Advanced IP Scanner" for Windows) to scan the network of the Pi. Once you find a device with the special name that you set in step 3, copy down its IP Address. Then use PuTTY to connect to the Pi. 
+
---
+++ setup.sh
@@ -0,0 +1,36 @@
+echo "This script updates and installs some necessary softwares. At the end, it will report its IP address for SSH"
+
+# Change apt-get source to TUNA
+sudo rm /etc/apt/sources.list
+echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib" >> /etc/apt/sources.list
+echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib" >> /etc/apt/sources.list
+sudo rm /etc/apt/sources.list.d/raspi.list
+echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main" >> /etc/apt/sources.list.d/raspi.list
+
+# Remove LibreOffice
+sudo apt-get -y remove --purge libreoffice*
+sudo apt-get -y clean
+sudo apt-get -y autoremove
+
+# Update softwares
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install some softwares
+sudo apt-get -y install apache2 python python3 git-all nmap python-pip
+
+# Enable SSH and VNC
+sudo systemctl enable ssh
+sudo systemctl enable vncserver-x11-serviced
+
+echo "Your IP address is: "
+ThisIP=`hostname -I`
+echo $ThisIP
+echo "Testing connection via nmap..." 
+echo `nmap -sP $ThisIP`
+
+git clone https://github.com/longyuxi/2019-genas-china-smart-device
+cd 2019-genas-china-smart-device
+
+## Under directory `2019-genas-china-smart-device`
+sudo pip install -r requirements.txt
